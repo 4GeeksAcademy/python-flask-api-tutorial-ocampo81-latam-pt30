a8c44405ddd16d8dff5264037edd9c5b4530cfd9
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-07-08 21:27:10 +0000

    First commit

---
+++ src/app.py
@@ -0,0 +1,34 @@
+   #Hice el paso a paso en un codespace que no podía hacer push porque era de 4geeks y luego
+#  hice el repo mío y solo copie y pegue mi resultado tras hacer todo el ejercicio para no tener que hacerlo dos veces
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [ { "label": "My first task", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+
+    todos.pop(position)
+
+    return jsonify(todos)
+
+# These two lines should always be at the end of your app.py file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
+
+#Hice el paso a paso en un codespace que no podía hacer push porque era de 4geeks y luego
+#  hice el repo mío y solo copie y pegue mi resultado tras hacer todo el ejercicio para no tener que hacerlo dos veces
